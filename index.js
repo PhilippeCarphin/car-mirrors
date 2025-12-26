f45cb1d60812b1eeb05f72e146d12387e4a865b8
Philippe Carphin <phil103@hotmail.com>
2025-12-25 22:20:37 -0500

    First commit

---
+++ index.js
@@ -0,0 +1,180 @@
+headBase = {x: 70, y:70}
+model = {
+    head: {x:headBase.x, y:headBase.y},
+    centerMirror: {pos: {x: 100, y: 30}, angle: 0.3, width: 20, curvature: 0},
+    leftMirror: {pos: {x: 40, y: 30}, angle: 0, width: 20, curvature: 0},
+    rightMirror: {pos: {x: 210, y: 30}, angle: 0.5, width: 20, curvature: 0},
+};
+
+
+canvas = document.getElementById("disp")
+centerSlider = document.getElementById("centerSlider")
+centerSlider.oninput = function () {
+    model.centerMirror.angle = this.value / 100.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+rightSlider = document.getElementById("rightSlider")
+rightSlider.oninput = function () {
+    model.rightMirror.angle = this.value / 100.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+leftSlider = document.getElementById("leftSlider")
+leftSlider.oninput = function () {
+    model.leftMirror.angle = this.value / 100.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+centerCurvatureSlider = document.getElementById("centerCurvatureSlider")
+centerCurvatureSlider.oninput = function () {
+    model.centerMirror.curvature = this.value / 20.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+rightCurvatureSlider = document.getElementById("rightCurvatureSlider")
+rightCurvatureSlider.oninput = function () {
+    model.rightMirror.curvature = this.value / 20.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+leftCurvatureSlider = document.getElementById("leftCurvatureSlider")
+leftCurvatureSlider.oninput = function () {
+    model.leftMirror.curvature = this.value / 20.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+
+centerWidthSlider = document.getElementById("centerWidthSlider")
+centerWidthSlider.oninput = function () {
+    model.centerMirror.width = this.value
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+rightWidthSlider = document.getElementById("rightWidthSlider")
+rightWidthSlider.oninput = function () {
+    model.rightMirror.width = this.value
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+leftWidthSlider = document.getElementById("leftWidthSlider")
+leftWidthSlider.oninput = function () {
+    model.leftMirror.width = this.value
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+
+headXOffset = document.getElementById("headXOffset")
+headXOffset.oninput = function () {
+    model.head.x = headBase.x + this.value / 3.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+headYOffset = document.getElementById("headYOffset")
+headYOffset.oninput = function () {
+    model.head.y = headBase.y + this.value / 3.0
+    ctx.clearRect(0, 0, canvas.height, canvas.width)
+    draw()
+}
+ctx = canvas.getContext("2d")
+
+function onCenterMirrorSlider() {
+
+}
+
+function normal(angle) {
+    return {x: -Math.sin(angle), y: Math.cos(angle)}
+}
+
+function endpoints(mirror){
+    c = Math.cos(mirror.angle)
+    s = Math.sin(mirror.angle)
+    return {
+        p0: {x: mirror.pos.x + c*mirror.width/2, y: mirror.pos.y + s*mirror.width/2},
+        p1: {x: mirror.pos.x - c*mirror.width/2, y: mirror.pos.y - s*mirror.width/2}
+    }
+}
+
+function drawMirror(mirror){
+    ep = endpoints(mirror)
+    drawLineSegment(ep.p0, ep.p1, "green", 3)
+    ctx.beginPath()
+    ctx.arc(ep.p0.x, ep.p0.y, 5, 0, 2*Math.PI)
+    ctx.arc(ep.p1.x, ep.p1.y, 5, 0, 2*Math.PI)
+    ctx.fillStyle = "red"
+    ctx.fill()
+}
+
+function drawHead(){
+    ctx.beginPath()
+    ctx.arc(model.head.x, model.head.y, 8, 0, 2*Math.PI)
+    ctx.fillStyle = "blue"
+    ctx.fill()
+}
+
+function drawMirrorArea(head, mirror, color) {
+    ctx.beginPath()
+    ctx.lineWidth = 1
+    ctx.strokeStyle = "black"
+    ctx.moveTo(head.x, head.y)
+    ctx.lineTo(mirror.pos.x, mirror.pos.y)
+    VC = {x: mirror.pos.x - head.x, y: mirror.pos.y - head.y}
+    RC = reflectDirection(VC, normal(mirror.angle))
+    ctx.lineTo(mirror.pos.x + 4*RC.x, mirror.pos.y + 4*RC.y)
+    ctx.stroke()
+
+    ctx.beginPath()
+    ctx.strokeStyle = color
+    ctx.lineWidth = 2
+    ep = endpoints(mirror)
+    ctx.moveTo(head.x, head.y)
+    ctx.lineTo(ep.p0.x, ep.p0.y)
+    V0 = {x: ep.p0.x - head.x, y: ep.p0.y - head.y}
+    R0 = reflectDirection(V0, normal(mirror.angle - mirror.curvature))
+    ctx.lineTo(ep.p0.x + 4*R0.x, ep.p0.y + 4* R0.y)
+
+    ctx.moveTo(head.x, head.y)
+    ctx.lineTo(ep.p1.x, ep.p1.y)
+    V1 = {x: ep.p1.x - head.x, y: ep.p1.y - head.y}
+    R1 = reflectDirection(V1, normal(mirror.angle + mirror.curvature))
+    ctx.lineTo(ep.p1.x + 4*R1.x, ep.p1.y + 4* R1.y)
+
+    ctx.stroke()
+}
+
+function reflectDirection(incident, normal){
+    dotProd = incident.x*normal.x + incident.y*normal.y
+    proj_x = dotProd * normal.x
+    proj_y = dotProd * normal.y
+    return {x: incident.x - 2*proj_x, y: incident.y - 2*proj_y}
+}
+
+function drawLineSegment(p0, p1, color, width){
+    ctx.strokeStyle = color
+    ctx.lineWidth = width
+    ctx.beginPath()
+    ctx.moveTo(p0.x, p0.y)
+    ctx.lineTo(p1.x, p1.y)
+    ctx.stroke()
+}
+
+
+function draw(){
+    drawHead()
+    drawMirror(model.leftMirror)
+    drawMirror(model.rightMirror)
+    drawMirror(model.centerMirror)
+    drawMirrorArea(model.head, model.centerMirror, "orange")
+    drawMirrorArea(model.head, model.leftMirror, "brown")
+    drawMirrorArea(model.head, model.rightMirror, "brown")
+}
+draw()
+
